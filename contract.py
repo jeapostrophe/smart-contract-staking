82574c4bc89699409fb107b18570bbed5671bb21
Jay McCarthy <jay.mccarthy@gmail.com>
2024-05-06 17:37:21 -0400

    Add comments to contract.py

--- contract.py
+++ contract.py
@@ -25,6 +25,7 @@ class SmartContractStaking(ARC4Contract):
     ##############################################
     def __init__(self) -> None:
         self.owner = Account()      # zero address
+        # JM: Since this is global.creator, you don't need to store it at all
         self.funder = Account()     # zero address
         self.period = UInt64()      # 0
         self.funding = UInt64()     # 0
@@ -41,6 +42,7 @@ class SmartContractStaking(ARC4Contract):
     ##############################################
     @arc4.abimethod
     def setup(self, owner: arc4.Address) -> None:
+        # JM: I don't think this function is useful and you should just put the enforce_step constraints in each function itself
         self.enforce_step(UInt64(0)) # Non-existant
         self.require_creator()
         self.funder = Txn.sender
@@ -60,6 +62,7 @@ class SmartContractStaking(ARC4Contract):
         self.enforce_step(UInt64(1)) # Fresh
         self.require_owner()
         assert period > 0, "period must be greater than 0" 
+        # JM: The Voi Foundation needs to verify that they are happy with this.
         assert period <= 5, "period must be less than or equal to 5"
         self.period = period.native
     ##############################################
@@ -80,6 +83,7 @@ class SmartContractStaking(ARC4Contract):
     def fill(self, total: arc4.UInt64, funding: arc4.UInt64) -> None:
         self.enforce_step(UInt64(2)) # Ready
         self.require_funder()
+        # JM: It is actually not necessary to have `total` as an argument that you check like this. Instead, you can inspect what the payment actually is and then that's what "total" is
         self.require_payment(self.funder, total.native)
         assert total > 0, "payment is greater than zero"
         self.total = total.native
@@ -142,6 +146,7 @@ class SmartContractStaking(ARC4Contract):
         self.require_owner()
         mab = self.calculate_mab()
         available_balance = self.get_available_balance()
+        # JM: You need to add the fee amount to the negative side OR set the fee of the itxn to 0 and enforce that the fee on the appl txn is 2
         assert available_balance - amount.native >= mab, "mab available"
         if amount > 0:
             itxn.Payment(
@@ -163,9 +168,11 @@ class SmartContractStaking(ARC4Contract):
     #   owner
     ##############################################
     @arc4.abimethod
+    # JM: I prefer to use the name `newOwner` to make sure we're not making a mistake
     def transfer(self, owner: arc4.Address) -> None:
         self.enforce_step(UInt64(3)) # Full
         self.require_owner()
+        # JM: Why bother?
         assert self.owner != owner.native, "new owner must not be owner"
         self.owner = owner.native
     ##############################################
@@ -187,13 +194,17 @@ class SmartContractStaking(ARC4Contract):
     ])
     def close(self) -> None:
         self.enforce_step(UInt64(3)) # Full
+        # JM: The spec does not require this to be called by the owner, because it is in the Foundation's interest to close it and get the resources back and there are no choices about how much or who to close to
         self.require_owner()
         assert self.calculate_mab() == 0, "mab is zero"
         oca = Txn.on_completion
+        # JM: We should not "allow" it to be delete; we should REQUIRE it to be "delete"
         if oca == OnCompleteAction.DeleteApplication:
             available_balance = self.get_available_balance()
             if available_balance > 0:
+                # JM: Do we need to handle the case where the balance is less than a fee, so we can't pay for a close out? I don't think so, because the foundation (which is the one that wants the app resources back) can send a fee and then make this happen
                 itxn.Payment(
+                    # JM: The receiver should be the owner, not the creator/funder
                     receiver=Global.creator_address,
                     close_remainder_to=self.owner
                 ).submit()
@@ -219,6 +230,7 @@ class SmartContractStaking(ARC4Contract):
     ##############################################
     @subroutine
     def require_payment(self, who: Account, amount: UInt64) -> None:
+        # JM: These 0s should be "my txn id - 1" to make the txn composable
         assert gtxn.PaymentTransaction(0).sender == who, "payment sender accurate"
         assert gtxn.PaymentTransaction(0).amount == amount, "payment amount accurate"
         assert gtxn.PaymentTransaction(0).receiver == Global.current_application_address, "payment receiver accurate"
@@ -279,6 +291,7 @@ class SmartContractStaking(ARC4Contract):
             case UInt64(2): # Ready
                 assert self.funder == Global.creator_address, "funder must be initialize"
                 assert self.owner != Global.zero_address, "owner must be initialized"
+                # JM: This "5" should be a global variable in the Python to reduce redundant redundancy
                 assert self.period <= 5, "period within bounds" 
                 assert self.funding == 0, "funding must not be initialized"
                 assert self.total == 0, "total must not be initialized"
@@ -304,6 +317,7 @@ class SmartContractStaking(ARC4Contract):
     ##############################################
     @subroutine
     def calculate_mab(self) -> UInt64:
+        # JM: You should make a normal Python script to run this function (with `now` as a parameter) with a bunch of different values to produce a CSV to produce a graph so the Foundation can look at it and ensure that it matches their expectations
         now = Global.latest_timestamp
         y = TemplateVar[UInt64]("VESTING_DELAY") # vesting delay
         seconds_in_period = TemplateVar[UInt64]("PERIOD_SECONDS") 
